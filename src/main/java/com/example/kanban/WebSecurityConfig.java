83028bf3b919d65258f3893c37573a44fb037f0f
Szymon Barszcz <szymon.barszcz99@gmail.com>
2020-04-17 06:53:50 +0200

    small fixes

--- src/main/java/com/example/kanban/WebSecurityConfig.java
+++ src/main/java/com/example/kanban/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/*").permitAll()
+                .antMatchers("/login","/register").permitAll()
                 .anyRequest().authenticated() // Każda inna podstrona wymaga autoryzacji
                 .and()
                 .formLogin()
