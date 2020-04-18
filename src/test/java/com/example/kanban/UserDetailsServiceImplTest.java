00a2bf8f59fcf2667112befec26118fd111157ca
dd0h <ddoh@o2.pl>
2020-04-18 17:24:22 +0200

    Small fix to pom.xml and UserDetailsServiceImplTest

--- src/test/java/com/example/kanban/UserDetailsServiceImplTest.java
+++ src/test/java/com/example/kanban/UserDetailsServiceImplTest.java
@@ -2,28 +2,19 @@ package com.example.kanban;
 
 
 import com.example.kanban.entities.user.User;
-import com.example.kanban.entities.user.UserDetailsImpl;
 import com.example.kanban.entities.user.UserRepository;
-import com.example.kanban.entities.user.UserType;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
 
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes={
         KanbanApplication.class,
         UserDetailsServiceImpl.class,
